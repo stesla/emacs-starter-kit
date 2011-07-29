226e7725105eff05f5e0b95b91e930372713faaa
Samuel Tesla <samuel.tesla@gmail.com>
2011-07-29 14:12:29 -0500

    remove unicode stuff

--- stesla/misc.el
+++ stesla/misc.el
@@ -1,3 +1,8 @@
 ;; My new favorite color theme
 (require 'zenburn)
 (color-theme-zenburn)
+
+;; I hate unicode substitutions
+(remove-hook 'coding-hook 'pretty-lambdas)
+(remove-hook 'espresso-mode-hook 'pretty-functions)
+(remove-hook 'js-mode-hook 'pretty-functions)
