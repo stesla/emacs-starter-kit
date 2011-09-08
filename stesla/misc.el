08e329b14ad66970b040ae0d9f78416ec1039a52
Samuel Tesla <stesla@drw.com>
2011-09-08 14:09:53 -0500

    turn off the unicode function symbol

--- stesla/misc.el
+++ stesla/misc.el
@@ -5,3 +5,5 @@
 ;; I hate unicode substitutions
 (remove-hook 'coding-hook 'pretty-lambdas)
 (remove-hook 'esk-js-mode-hook 'pretty-functions)
+(eval-after-load 'clojure-mode
+  '(remove-hook 'clojure-mode-hook 'esk-pretty-fn))
